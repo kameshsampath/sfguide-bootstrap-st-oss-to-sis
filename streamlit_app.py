86ed946b0c5d22130433163148d6a0b426fc03ff
Kamesh Sampath <kamesh.sampath@hotmail.com>
2024-11-13 15:05:52 +0530

    (fix): Sidebar with Features

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,3 +28,63 @@ with st.expander("Data"):
         y="body_mass_g",
         color="species",
     )
+
+# Ineractivity
+# Columns:
+# 'species', 'island', 'bill_length_mm', 'bill_depth_mm',
+# 'flipper_length_mm', 'body_mass_g', 'sex'
+with st.sidebar:
+    st.header("Input Features")
+    islands = df.island.unique().astype(str)
+    island = st.selectbox(
+        "Island",
+        islands,
+    )
+    gender = st.radio(
+        "Gender",
+        ("Male", "Female"),
+    )
+    min, max, mean = (
+        df.bill_length_mm.min(),
+        df.bill_length_mm.max(),
+        df.bill_length_mm.mean().round(2),
+    )
+    bill_length_mm = st.slider(
+        "Bill Length(mm)",
+        min_value=min,
+        max_value=max,
+        value=mean,
+    )
+    min, max, mean = (
+        df.bill_depth_mm.min(),
+        df.bill_depth_mm.max(),
+        df.bill_depth_mm.mean().round(2),
+    )
+    bill_depth_mm = st.slider(
+        "Bill Depth(mm)",
+        min_value=min,
+        max_value=max,
+        value=mean,
+    )
+    min, max, mean = (
+        df.filpper_length_mm.min(),
+        df.filpper_length_mm.max(),
+        df.filpper_length_mm.mean().round(2),
+    )
+    filpper_length_mm = st.slider(
+        "Filpper Length(mm)",
+        min_value=min,
+        max_value=max,
+        value=mean,
+    )
+    min, max, mean = (
+        df.filpper_length_mm.min(),
+        df.filpper_length_mm.max(),
+        df.filpper_length_mm.mean().round(2),
+    )
+    body_mass_g = st.slider(
+        "Body Mass(g)",
+        min_value=min,
+        max_value=max,
+        value=mean,
+    )
